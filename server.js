9e919d2159597b8a439cf5ca138c88a014597616
Eric Maciel <elm2178@columbia.edu>
2017-09-18 00:15:14 -0400

    Added, bcrypt, session, morgan, and passport

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var app = require('express')();
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://admin:admin@cluster0-shard-00-00-d2tbf.mongodb.net:27017,cluster0-shard-00-01-d2tbf.mongodb.net:27017,cluster0-shard-00-02-d2tbf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin", {useMongoClient: true});
+mongoose.Promise = global.Promise;
+var morgan = require('morgan');
+var passport = require('passport');
+var session = require('express-session');
+var bcrypt = require('bcrypt-nodejs');
 
-var User = mongoose.model('user', { username: String, password: String, email: String });
+mongoose.connect("mongodb://admin:admin@cluster0-shard-00-00-d2tbf.mongodb.net:27017,cluster0-shard-00-01-d2tbf.mongodb.net:27017,cluster0-shard-00-02-d2tbf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin", {useMongoClient: true});
+var User = mongoose.model('user', { local: { username: String, password: String, email: String }});
+
+// set up express
+app.use(morgan('dev'));
+app.use(session({ secret: 'ilovesarahmaciel', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(req, res) {
+  res.send("Hello, world");
+});
+
+app.get('/users', function(req, res) {
   User.find().lean().exec(function (err, users) {
     return res.end(JSON.stringify(users));
   });
